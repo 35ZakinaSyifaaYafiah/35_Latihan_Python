c834e7618bbb252c2fa2782bea61f6c0a32af385
35_ZakinaSyifa'aYafi'ah <zakina.tkjsmeksa25@gmail.com>
2026-09-04 12:50:02 +0700

    modul_bangun_datar

---
+++ modul_bangun_datar.py
@@ -0,0 +1,11 @@
+def proses_luas_persegi(sisi):
+    return sisi * sisi
+
+def proses_keliling_persegi(sisi):
+    return 4 * sisi
+
+def proses_luas_lingkaran(r):
+    return 3.14 * r * r
+
+def proses_luas_segitiga(alas, tinggi):
+    return 0.5 * alas * tinggi
